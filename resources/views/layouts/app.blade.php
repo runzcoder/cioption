b30947a358a0cc44e96d2196275175c26c7703a9
runzcoder <runzcoder@gmail.com>
2023-01-30 01:51:47 +0100

    saved

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/sass/app.css', 'resources/js/app.js'])
 
     <script src="//code.jivosite.com/widget/1GEO6kFpzo" async></script>
 
